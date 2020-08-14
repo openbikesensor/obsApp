d2c967922ccc62303c509e20ee965aa3fdef0bed
hpcwoess <woessner@hlrs.de>
2020-08-14 22:18:23 +0200

    added missing files

---
+++ src/app/core/services/trackData.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ApiService } from './api.service';
+import { TrackData } from '../models';
+import { map } from 'rxjs/operators';
+
+
+@Injectable()
+export class TrackDataService {
+    private trackData: TrackData;
+  constructor (
+    private apiService: ApiService
+  ) {}
+
+  getAll(slug): Observable<TrackData> {
+    return this.apiService.get(`/tracks/${slug}/TrackData`).pipe(map(data => data.trackData));
+  }
+  getTrackData(slug): TrackData {
+    this.apiService.get(`/tracks/${slug}/TrackData`).pipe(map(data => data.trackData)).subscribe(trackData => this.trackData = trackData);
+    return this.trackData;
+  }
+
+}
